6d27b387358eced77fd15986671b9c293fce0ac5
Mohamed Ayman <mohammed.a.alhadad@gmail.com>
2025-03-18 05:16:43 +0200

    store, axiosInstance

--- Dashboard/src/Utils/axiosInstance.js
+++ Dashboard/src/Utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"",
+    baseURL:"https://hatly-api.onrender.com",
     headers:{
         "Content-Type":"application/json",
     }
